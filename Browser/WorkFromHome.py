00387d4a8456db1cd67ae874c86590886e550faa
Laurent Tupin <laurent.tupin@ihsmarkit.com>
2021-03-08 15:50:08 +0800

    work from home

---
+++ Browser/WorkFromHome.py
@@ -0,0 +1,20 @@
+# How to keep your computer active so your boss does not notice you are away
+
+import time
+import pyautogui
+pyautogui.FAILSAFE = False
+
+while True:
+    time.sleep(10)
+    # move mouse
+    for i in range(10):
+        o_pos = pyautogui.position()
+        if o_pos.y > 1000:      pyautogui.moveTo(o_pos.x , 0)
+        elif o_pos.x > 1000:    pyautogui.moveTo(0 , o_pos.y)
+        else:                   pyautogui.moveTo(o_pos.x + 10, o_pos.y + 50)
+        print(o_pos.x, o_pos.y)
+    # Press Shift
+    print('------------------')
+    pyautogui.press('shift')
+    
+    
